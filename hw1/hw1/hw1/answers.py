89cb00e935fb8c1409c28c389134a3d7df1ea486
liorrofeim <liorrofeim@gmail.com>
2024-04-24 13:55:21 +0300

    implementation till section 2

---
+++ hw1/hw1/hw1/answers.py
@@ -0,0 +1,130 @@
+r"""
+Use this module to write your answers to the questions in the notebook.
+
+Note: Inside the answer strings you can use Markdown format and also LaTeX
+math (delimited with $$).
+"""
+
+student_name_1 = 'LEEOR ROFEIM' # string
+student_ID_1 = '212356398' # string
+student_name_2 = '' # string
+student_ID_2 = '' # string
+
+
+# ==============
+# Part 1 answers
+
+part1_q1 = r"""
+**Your answer:**
+
+1. False. The test set is used to estimate the model's out-of-sample error, not the in-sample error.
+the in-sample error is calculated using the training set.
+
+
+2. False. not all splits are equally useful. The split as to be random. And the training set should have enough data to train the model. (commonly 70-30, 80-20) 
+
+3. True. We can think of cross-validation as part of the tarining so we will not use test set during cross-validation
+bescause we want to keep the model unbiased in respect to the test set.
+
+4. True ,running the model on the validation-set provide an estimate of how the model will perform on unseen data
+"""
+
+part1_q2 = r"""
+**Your answer:**
+
+
+No, as we mentioned earlier the test set is only used once and this is only to estimate the models performance on unseen data.
+Using test set for hyperparameter tuning may couse overfitting.
+Insted we can split the training set to training and validation set and use the validation set for hyperparameter tuning.
+
+"""
+
+# ==============
+# Part 2 answers
+
+part2_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part2_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part2_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+# ==============
+
+# ==============
+# Part 3 answers
+
+part3_q1 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q2 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+part3_q3 = r"""
+**Your answer:**
+
+
+Write your answer using **markdown** and $\LaTeX$:
+```python
+# A code block
+a = 2
+```
+An equation: $e^{i\pi} -1 = 0$
+
+"""
+
+# ==============
+
+# ==============
---
+++ hw1/hw1/hw1/losses.py
@@ -0,0 +1,99 @@
+import abc
+import torch
+
+
+class ClassifierLoss(abc.ABC):
+    """
+    Represents a loss function of a classifier.
+    """
+
+    def __call__(self, *args, **kwargs):
+        return self.loss(*args, **kwargs)
+
+    @abc.abstractmethod
+    def loss(self, *args, **kw):
+        pass
+
+    @abc.abstractmethod
+    def grad(self):
+        """
+        :return: Gradient of the last calculated loss w.r.t. model
+            parameters, as a Tensor of shape (D, C).
+        """
+        pass
+
+
+class SVMHingeLoss(ClassifierLoss):
+    def __init__(self, delta=1.0):
+        self.delta = delta
+        self.grad_ctx = {}
+
+    def loss(self, x, y, x_scores, y_predicted):
+        """
+        Calculates the Hinge-loss for a batch of samples.
+
+        :param x: Batch of samples in a Tensor of shape (N, D).
+        :param y: Ground-truth labels for these samples: (N,)
+        :param x_scores: The predicted class score for each sample: (N, C).
+        :param y_predicted: The predicted class label for each sample: (N,).
+        :return: The classification loss as a Tensor of shape (1,).
+        """
+
+        assert x_scores.shape[0] == y.shape[0]
+        assert y.dim() == 1
+
+        # TODO: Implement SVM loss calculation based on the hinge-loss formula.
+        #  Notes:
+        #  - Use only basic pytorch tensor operations, no external code.
+        #  - Full credit will be given only for a fully vectorized
+        #    implementation (zero explicit loops).
+        #    Hint: Create a matrix M where M[i,j] is the margin-loss
+        #    for sample i and class j (i.e. s_j - s_{y_i} + delta).
+
+        loss = None
+        # ====== YOUR CODE: ======
+        N = x_scores.size(0)
+        # Gather the scores for the correct classes according to y
+        correct_class_scores = x_scores[torch.arange(N), y].view(-1, 1)  # Shape (N, 1)
+
+        # Calculate margins for all class scores with respect to the correct class scores
+        margins = x_scores - correct_class_scores + self.delta
+
+        # Ensure the margin for the correct class is 0
+        margins[torch.arange(N), y] = 0
+
+        # Calculate the hinge loss
+        loss = torch.max(margins, torch.zeros_like(margins)).sum() / N
+        # ========================
+
+        # TODO: Save what you need for gradient calculation in self.grad_ctx
+        # ====== YOUR CODE: ======
+        self.grad_ctx = {
+            'x': x,
+            'y': y,
+            'x_scores': x_scores,
+            'y_predicted': y_predicted,
+            'margins': margins
+        }
+
+        # ========================
+
+        return loss
+
+    def grad(self):
+        """
+        Calculates the gradient of the Hinge-loss w.r.t. parameters.
+        :return: The gradient, of shape (D, C).
+
+        """
+        # TODO:
+        #  Implement SVM loss gradient calculation
+        #  Same notes as above. Hint: Use the matrix M from above, based on
+        #  it create a matrix G such that X^T * G is the gradient.
+
+        grad = None
+        # ====== YOUR CODE: ======
+        raise NotImplementedError()
+        # ========================
+
+        return grad
